06d225404f83fe2037c9f149e501a93deae3441b
avincze <avincze73@gmail.com>
2019-10-27 19:07:04 +0100

    a

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome Fibonacci series calculation</h1>
+            <h1 className="App-title">Welcome Fibonacci series calculation KUBERNETES</h1>
             <Link to="/">Home</Link>
             <Link to="/otherpage">Link to other page</Link>
           </header>
